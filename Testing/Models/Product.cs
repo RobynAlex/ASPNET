ecbd1f52e0fd9d27b855aa37c25b420a28385db5
RobynAlex <robyn.alexander1111@gmail.com>
2022-02-24 19:03:43 -0600

    full commit

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class Product : IProductRepository
+    {
+        public Product()
+        { }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public Product AssignCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertProduct(Product productToInsert)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
